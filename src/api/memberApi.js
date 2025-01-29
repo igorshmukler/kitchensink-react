a8d1a51994132d157613d4e54f62275bf69ee48c
Igor Shmukler <igor.shmukler@gmail.com>
2025-01-28 23:22:36 -0500

    cleanup

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -1,9 +1,9 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = 'http://localhost:8080'
 
 export async function getMembers() {
   const response = await fetch(`${BASE_URL}/members`)
   if (!response.ok) {
-    throw new Error("Failed to fetch members")
+    throw new Error('Failed to fetch members')
   }
   return await response.json()
 }
@@ -11,7 +11,7 @@ export async function getMembers() {
 export async function getMemberbyId(id) {
   const response = await fetch(`${BASE_URL}/members/${id}`)
   if (!response.ok) {
-    throw new Error("Failed to fetch member")
+    throw new Error('Failed to fetch member')
   }
   return await response.json()
 }
@@ -20,34 +20,34 @@ export async function addMember(member) {
   const response = await fetch(`${BASE_URL}/members`, {
     method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(member),
   })
   if (!response.ok) {
-    throw new Error("Failed to add member")
+    throw new Error('Failed to add member')
   }
 }
 
 export async function updateMember(id, member) {
   const response = await fetch(`${BASE_URL}/members/${id}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(member),
   })
   if (!response.ok) {
-    throw new Error("Failed to update member")
+    throw new Error('Failed to update member')
   }
   return await response.json()
 }
 
 export async function deleteMember(id) {
   const response = await fetch(`${BASE_URL}/members/${id}`, {
-    method: "DELETE",
+    method: 'DELETE',
   })
   if (!response.ok) {
-    throw new Error("Failed to delete member")
+    throw new Error('Failed to delete member')
   }
 }
